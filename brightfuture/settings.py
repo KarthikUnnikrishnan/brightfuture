956d42eb514fba31f0f23bc64526ef883917baa3
Karthik Unnikrishnan <karthikunnikrishnan2003@gmail.com>
2024-12-06 12:52:56 +0530

    home-section-left-div

--- brightfuture/settings.py
+++ brightfuture/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
