01baf832423cc3ac60dbdbdd7310e272c7774f2a
Karthik Unnikrishnan <karthikunnikrishnan2003@gmail.com>
2024-12-07 19:25:37 +0530

    why-choose-section-addition

--- base_app/static/js/home.js
+++ base_app/static/js/home.js
@@ -1,1 +1,19 @@
-console.log("Testing")
+/* Navbar expands and shrinking */
+
+window.onload = function() {
+    scrollFunction(); // Set the initial state of the navbar on page load
+};
+
+window.onscroll = function() {
+    scrollFunction(); // Update the navbar style on scroll
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar").style.margin = "0";
+        document.getElementById("navbar").style.borderRadius = "0";
+    } else {
+        document.getElementById("navbar").style.margin = "40px 120px";
+        document.getElementById("navbar").style.borderRadius = "20px";
+    }
+}
