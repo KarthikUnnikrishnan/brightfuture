4b7fa0719d5caa7b63db2e0723c194a7071e0187
Karthik Unnikrishnan <karthikunnikrishnan2003@gmail.com>
2024-12-08 20:28:24 +0530

    page-loader-addition

--- base_app/static/js/home.js
+++ base_app/static/js/home.js
@@ -1,19 +1,31 @@
 /* Navbar expands and shrinking */
 
 window.onload = function() {
-    scrollFunction(); // Set the initial state of the navbar on page load
+scrollFunction(); 
 };
 
 window.onscroll = function() {
-    scrollFunction(); // Update the navbar style on scroll
+scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("navbar").style.margin = "0";
-        document.getElementById("navbar").style.borderRadius = "0";
-    } else {
-        document.getElementById("navbar").style.margin = "40px 120px";
-        document.getElementById("navbar").style.borderRadius = "20px";
-    }
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.margin = "0";
+    document.getElementById("navbar").style.borderRadius = "0";
+} else {
+    document.getElementById("navbar").style.margin = "40px 120px";
+    document.getElementById("navbar").style.borderRadius = "20px";
 }
+}
+
+/* Page loader script */
+
+window.addEventListener("load", () => {
+const loader = document.querySelector(".loader");
+setTimeout(() => {
+    loader.classList.add("loader--hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+    });
+}, 1500); // 1000 milliseconds = 1 seconds
+});
