9c38c8c6fd3af19dceb4fe32386838b02f702997
Karthik Unnikrishnan <karthikunnikrishnan2003@gmail.com>
2025-01-23 21:11:22 +0530

    empty commit

--- base_app/views.py
+++ base_app/views.py
@@ -4,5 +4,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'base_app\home.html')
 
-def dropout_prediction(request):
-    return render(request, 'base_app/dropout_form.html')
