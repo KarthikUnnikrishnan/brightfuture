7af55b6a3ce76a1f988ab8aaf7182689d25e364f
Karthik Unnikrishnan <karthikunnikrishnan2003@gmail.com>
2025-01-25 16:36:45 +0530

    Prediction_model_partially_implemented

--- base_app/urls.py
+++ base_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('predict/', views.predict_dropout, name='predict_dropout'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import joblib
 
 # Create your views here.
-def home(request):
-    return render(request, 'base_app\home.html')
+def home(request, prediction=None):
+    return render(request, 'base_app/home.html', {'prediction': prediction})
 
+def predict_dropout(request):
+    prediction = None
+    if request.method == 'POST':
+        first_internal = request.POST.get('1st-internal')
+        second_internal = request.POST.get('2nd-internal')
+        tuition_status = request.POST.get('tuition-input')
+        attendance_status = request.POST.get('attendence-input')
+
+        # Data Preparation
+        input_data = [[first_internal, second_internal, tuition_status, attendance_status]]
+
+        # Making prediction
+        model = joblib.load('ml_models/dropout_prediction/dropout-model.joblib')
+        prediction = model.predict(input_data)
+
+        # Render home.html with prediction result
+        return render(request, 'base_app/home.html', {'prediction': prediction[0]})
